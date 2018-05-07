b1ac145d8cbdab14a088d4c37bf4fa808dc85b5f
DarkHaliax96 <angel.r.ruiz.m@gmail.com>
2018-05-06 21:16:08 -0500

    Added user image model and data service that connects to mongo

---
+++ FrontEnd/src/app/models/user_image.ts
@@ -0,0 +1,6 @@
+export class User_image {
+    constructor(
+        public user_id: string,
+        public path: string
+    ) { }
+}
--- FrontEnd/src/app/services/data.service.ts
+++ FrontEnd/src/app/services/data.service.ts
@@ -1,8 +1,45 @@
 import { Injectable } from '@angular/core';
+import { AuthService } from './auth.service';
+import 'rxjs/Rx';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable()
 export class DataService {
 
-  constructor() { }
+  URL: string;
+  headers: HttpHeaders;
+
+  constructor(private auth: AuthService, private http: HttpClient) {
+    this.URL = 'http://10.50.65.22:3500/userImage';
+
+    this.headers = new HttpHeaders({
+      'Content-Type': 'application/json'
+    });
+
+  }
+
+  retrive(id: string){
+    return this.http.get(
+      this.URL + '/' + id, {headers: this.headers}
+    );
+  }
+
+  create(body: any){
+    return this.http.post(
+      this.URL, body, {headers: this.headers}
+    );
+  }
+
+  update(id: string, body: any){
+    return this.http.post(
+      this.URL + '/' + id, body, {headers: this.headers}
+    );
+  }
+
+  delete(id:string){
+    return this.http.delete(
+      this.URL + '/' + id, {headers: this.headers}
+    )
+  }
 
 }
